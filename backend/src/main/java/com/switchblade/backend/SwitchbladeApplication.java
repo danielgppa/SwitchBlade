7893a2e9288a6a548a4882527330f6d821cd6faa
danielgppa <95982867+danielgppa@users.noreply.github.com>
2023-12-13 21:18:25 -0300

    Backend initial setup

---
+++ backend/src/main/java/com/switchblade/backend/SwitchbladeApplication.java
@@ -0,0 +1,13 @@
+package com.switchblade.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchbladeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchbladeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/switchblade/backend/SwitchbladeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchblade.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchbladeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
