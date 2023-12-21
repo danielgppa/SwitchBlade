79f5c7bb0cbdd271596472b8aa25567ad8cae6b7
danielgppa <95982867+danielgppa@users.noreply.github.com>
2023-12-21 20:17:25 -0300

    Remove unused code and update API import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Card from './components/Card';
 import MainScreen from './pages/MainScreen';
 import Calculators from './pages/Calculators';
 import Generators from './pages/Generators';
 import NotFound from './pages/NotFound';
 import HeaderSB from './components/HeaderSB';
-import Test from './pages/Test';
 import { BrowserRouter as Router, Route, Routes, Switch, Redirect } from 'react-router-dom'
 
 function App() {
@@ -18,7 +16,6 @@ function App() {
           <Route exact path='/' component={MainScreen} />
           <Route path='/calculators' component={Calculators} />
           <Route path='/generators' component={Generators} />
-          <Route path='/test' component={Test} />
           <Route path='/404' component={NotFound} />
           <Redirect from='*' to='/404' />
         </Switch>
